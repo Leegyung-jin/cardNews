c90e40c7df1efd9a6b4a73c6dfca920269f16346
LeeGyung-jin <0l055225757@gmail.com>
2022-03-10 23:10:38 +0900

    ## SpringBoot, MySQL Setting

---
+++ src/main/java/com/gourmap/GourmapApplication.java
@@ -0,0 +1,13 @@
+package com.gourmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmapApplication.class, args);
+	}
+
+}
